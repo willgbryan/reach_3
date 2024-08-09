c51c0ff253646baec9226fecdcab358e10990e43
willgbryan <will.bryan421@gmail.com>
2024-08-08 20:27:46 -0500

    more route changes

--- frontend/src/app/auth/sign-out/route.ts
+++ frontend/src/app/auth/sign-out/route.ts
@@ -6,6 +6,7 @@ import { createClient } from '@/db/server'
 export async function POST(req: NextRequest) {
   const cookieStore = cookies()
   const supabase = createClient(cookieStore)
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://themagi.systems';
 
   // Check if we have a session
   const {
@@ -16,7 +17,7 @@ export async function POST(req: NextRequest) {
     await supabase.auth.signOut()
   }
 
-  return NextResponse.redirect(new URL('/auth/sign-in', req.url), {
+  return NextResponse.redirect(new URL(`${baseUrl}/auth/sign-in`, req.url), {
     status: 302,
   })
 }
